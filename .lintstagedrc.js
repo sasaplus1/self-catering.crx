829eb489b032d04bbb554469c0e8cb8894da32ed
sasaplus1 <sasaplus1@gmail.com>
2020-02-13 11:51:59 +0900

    add husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  '*.js': 'npx eslint --cache',
+  '*.yml': [
+    'npx prettier --parser yaml --write',
+    'git diff --exit-code --quiet'
+  ],
+  'package.json': [
+    'npx fixpack',
+    'npx prettier --parser json-stringify --write',
+    'git diff --exit-code --quiet'
+  ],
+  'package-lock.json': 'node -e "process.exitCode = 1;"'
+};
